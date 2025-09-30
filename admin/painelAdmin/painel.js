08173ea0bd5b3d022fe39dd06d64282298266e7c
MusicStyle <music.style118412@gmail.com>
2025-09-30 07:54:53 -0300

    Deletado directório admin

--- admin/painelAdmin/painel.js
+++
@@ -1,8 +0,0 @@
-if (sessionStorage.getItem("logado") !== "true") {
-  alert("Acesso não autorizado. Faça login primeiro.");
-  window.location.href = "admin/index.html";
-}
-function logout() {
-  sessionStorage.removeItem("logado");
-  window.location.href = "admin/index.html";
-}
