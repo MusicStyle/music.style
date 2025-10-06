3c69b6595b76c536fe4c73eaf5c85a1bd112263d
Gutooo303 <augusto.santos124@etec.sp.gov.br>
2025-10-06 13:37:59 +0000

    Alterado o estilo do site; Feito o sistema de login; Melhorado o estilo do home.html.

---
+++ script/login.js
@@ -0,0 +1,25 @@
+let usuario = {
+    nome: "",
+    email: "",
+    senha: ""
+};
+
+document.getElementById("loginBtn").addEventListener("click", function () {
+    usuario.nome = document.getElementById("loginName").value;
+    usuario.senha = document.getElementById("loginPassword").value;
+    console.log(usuario);
+    window.location.href = "home.html";
+});
+
+    document.getElementById("registerBtn").addEventListener("click", function () {
+    usuario.nome = document.getElementById("registerName").value;
+    usuario.email = document.getElementById("registerEmail").value;
+    usuario.senha = document.getElementById("registerPassword").value;
+    console.log(usuario);
+    window.location.href = "home.html";
+});
+if (usuario.nome && usuario.senha) {
+    window.location.href = "home.html";
+} else {
+    alert("Por favor, preencha todos os campos!");
+}
